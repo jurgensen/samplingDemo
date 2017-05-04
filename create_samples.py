d20577a8079928e1583bf59801d1ab791ed76f48
Anna E Jurgensen <jurgensen.anna@gmail.com>
2017-05-03 22:32:45 -0700

    added function for number input
    added function that allows the use to input their own values for
    population size, rate of responsiveness, sample size, and number of
    samples.  The output charts are saved to a file. To run, just need to
    import/source the file and call run_samples()

--- create_samples.py
+++ create_samples.py
@@ -1,5 +1,9 @@
 import re
 import random
+import matplotlib.pyplot as plt
+#from matplotlib.gridspec import subplot2grid
+#from matplotlib.gridspec import GridSpec
+#import matplotlib.patches as patches
 
 def add_doc_type(number, doctype):
 	if re.match('non', doctype):
@@ -48,25 +52,65 @@ def get_mean(a_list):
 #def get_RMS():
 #def get_STD():
 
-pop_resp = 30000.0
-pop_total = 130000.0
-sample_size = 100
-number_samples = 25.0
 
-population = create_population(pop_resp,(pop_total-pop_resp))
-prevalence = float(pop_resp/pop_total)
-print('actual prevalence: {:f}'.format(prevalence))
+pop_resp = 0.0
+pop_total = 0.0
+sample_size = 300 #default size is 300
+number_samples = 25.0 #default number is 25
 
+def get_numbers():
+    total = float(input('Enter total population size: '))
+    resp = float(input('Enter responsive (sub)population size: '))
+    size = int(input('Enter the sample size: '))
+    number = float(input('Enter the number of samples: '))
+    return total, resp, size, number
 
+def run_samples():
+    [pop_total, pop_resp, sample_size, number_samples] = get_numbers()
+    population = create_population(pop_resp,(pop_total-pop_resp))
+    prevalence = float(pop_resp/pop_total)
+    print('actual prevalence: {:.2f}'.format(prevalence))
+    fracs = [100.0*pop_resp/pop_total, 100.0*(pop_total-pop_resp)/pop_total]
+    avg_responsiveness = 100.0*get_mean(get_many_samples(population, sample_size, number_samples))
+    sample_fracs = [avg_responsiveness, 100-avg_responsiveness]
 
+    fig1 = plt.figure()
+    #ax1 = fig1.add_subplot(121, aspect = 'equal')
+    ax1 = plt.subplot2grid((2,6),(0,0), rowspan = 2, colspan = 3, aspect = 'equal')
+    labels = ['R ({:.0f})'.format(pop_resp), 'NR ({:.0f})'.format(pop_total-pop_resp)]
+    ax1.pie(fracs, labels=labels, colors=['red','blue'], autopct='%.0f%%')
+
+    #ax2 = fig1.add_subplot(122, aspect = 'equal')
+    ax2 = plt.subplot2grid((2,6),(0,4), aspect = 'equal')
+    labels = ['R ({:.0f})'.format(avg_responsiveness*sample_size/100), 'NR ({:.0f})'.format(sample_size-(avg_responsiveness*sample_size/100))]
+    ax2.pie(sample_fracs, labels=labels, colors=['red','blue'], autopct='%.0f%%')
+
+    plt.tight_layout()
+
+    fig1.savefig('sample_charts.png')
+
+    
+  
+'''
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111,aspect = 'equal')
+ax1.add_patch(patches.Rectangle((0.5,0.5), 1, 1))
+plt.axis('equal')
+plt.axis('off')
+#fig1.savefig('rect1.png', dpi= 90, bbox_inches='tight')
+#plt.show()
+fig1.savefig('your_graph.png')
+'''
+'''
 round_one = get_many_samples(population, sample_size, number_samples)
-print(round_one)
+#print(round_one)
 mean = get_mean(round_one)
 difference = abs(mean - prevalence)
-print(mean)
-print(difference)
+print('{:.3f}'.format(mean))
+print('{:.3f}'.format(difference))
 print(' ')
-
+'''
+'''
 sample_size = 1000
 number_samples = 25
 round_two = get_many_samples(population, sample_size, number_samples)
@@ -75,4 +119,4 @@ mean = get_mean(round_two)
 difference = abs(mean - prevalence)
 print(mean)
 print(difference)
-
+'''
